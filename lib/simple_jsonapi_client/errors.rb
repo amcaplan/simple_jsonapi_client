e2d8e0dfd96ed406339a2dda613fdcb041bc4657
Ariel Caplan <ariel.caplan@vitals.com>
2017-08-14 00:23:56 +0300

    Properly namespace error
    It wasn't being found by SimpleJSONAPIClient::Base

--- lib/simple_jsonapi_client/errors.rb
+++ lib/simple_jsonapi_client/errors.rb
@@ -1,69 +1,71 @@
 require 'delegate'
 
 module SimpleJSONAPIClient
-  class Error < StandardError
-  end
-
-  class ApiError < Error
-    extend Forwardable
-
-    attr_reader :response
-    def_delegators :response, :status, :body
-
-    def initialize(response)
-      @response = response
-      super(full_message)
+  class Base
+    class Error < StandardError
     end
 
-    def errors
-      Array(body['errors'])
-    end
+    class ApiError < Error
+      extend Forwardable
 
-    def message
-      if !codes.empty?
-        codes_message
-      elsif !details.empty?
-        details_message
-      else
-        default_message
+      attr_reader :response
+      def_delegators :response, :status, :body
+
+      def initialize(response)
+        @response = response
+        super(full_message)
       end
-    end
 
-    def full_message
-      "The API returned a #{status} error status and this content:\n" +
-        pretty_printed_response.each_line.map { |line| "  #{line}" }.join
-    end
+      def errors
+        Array(body['errors'])
+      end
 
-    def codes
-      @codes ||= errors.map { |error| error['code'] }.compact
-    end
+      def message
+        if !codes.empty?
+          codes_message
+        elsif !details.empty?
+          details_message
+        else
+          default_message
+        end
+      end
 
-    def details
-      @details ||= errors.map { |error| error['detail'] }.compact
-    end
+      def full_message
+        "The API returned a #{status} error status and this content:\n" +
+          pretty_printed_response.each_line.map { |line| "  #{line}" }.join
+      end
 
-    private
+      def codes
+        @codes ||= errors.map { |error| error['code'] }.compact
+      end
 
-    def pretty_printed_response
-      JSON.pretty_generate(body)
-    end
+      def details
+        @details ||= errors.map { |error| error['detail'] }.compact
+      end
 
-    def codes_message
-      codes_word = codes.size == 1 ? 'code' : 'codes'
-      "The API returned a #{status} error status with the following error #{codes_word}: #{
+      private
+
+      def pretty_printed_response
+        JSON.pretty_generate(body)
+      end
+
+      def codes_message
+        codes_word = codes.size == 1 ? 'code' : 'codes'
+        "The API returned a #{status} error status with the following error #{codes_word}: #{
         codes.map(&:inspect).join(', ')
-      }"
-    end
+        }"
+      end
 
-    def details_message
-      details_word = details.size == 1 ? 'detail' : 'details'
-      "The API returned a #{status} error status with the following error #{details_word}: #{
+      def details_message
+        details_word = details.size == 1 ? 'detail' : 'details'
+        "The API returned a #{status} error status with the following error #{details_word}: #{
         details.map(&:inspect).join(', ')
-      }"
-    end
+        }"
+      end
 
-    def default_message
-      "The API responded with a #{status} error status."
+      def default_message
+        "The API responded with a #{status} error status."
+      end
     end
   end
 end
