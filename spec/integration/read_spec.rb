659c08da2452b95387921c1c315a991022936110
Ariel Caplan <ariel.caplan@vitals.com>
2017-08-14 23:10:49 +0300

    Test reading relationships

--- spec/integration/read_spec.rb
+++ spec/integration/read_spec.rb
@@ -18,6 +18,14 @@ RSpec.describe 'reading models' do
     )
   end
 
+  def create_post(author:, title:, text:)
+    JSONAPIAppClient::Post.create(
+      attributes: { title: title, text: text },
+      relationships: { author: author },
+      connection: connection
+    )
+  end
+
   context 'fetching an individual model' do
     context 'using a nonexistent id' do
       let(:id) { 'foobar' }
@@ -28,11 +36,32 @@ RSpec.describe 'reading models' do
     end
 
     context 'using an id that exists' do
-      let(:id) { create_author.id }
+      let(:author) { create_author }
+      let(:id) { author.id }
       let(:name) { 'Macadamia' }
 
       it 'finds the model' do
         expect(fetch_author(id).name).to eq(name)
+      end
+
+      context 'fetching plural relationships' do
+        context 'Given the relationships are absent' do
+          it 'returns an empty Array' do
+            expect(fetch_author(id).posts).to be_empty
+          end
+        end
+
+        context 'Given the relationships are present' do
+          let!(:post) { create_post(author: author, title: title, text: text) }
+          let(:title) { 'The System is Down' }
+          let(:text) { 'The Cheat' }
+
+          it 'returns the related models' do
+            posts = fetch_author(id).posts
+            expect(posts.length).to eq(1)
+            expect(posts.first).to be_same_record_as(post)
+          end
+        end
       end
     end
   end
