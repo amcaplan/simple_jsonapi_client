2df995711872124f60c3de0ba05385a0415337d7
Ariel Caplan <ariel.caplan@vitals.com>
2017-08-14 23:01:01 +0300

    Assert that reads work

--- spec/integration/read_spec.rb
+++ spec/integration/read_spec.rb
@@ -11,12 +11,28 @@ RSpec.describe 'reading models' do
     )
   end
 
+  def create_author
+    JSONAPIAppClient::Author.create(
+      attributes: { name: name },
+      connection: connection
+    )
+  end
+
   context 'fetching an individual model' do
     context 'using a nonexistent id' do
       let(:id) { 'foobar' }
 
       it 'raises a NotFoundError' do
         expect { fetch_author(id) }.to raise_error { SimpleJSONAPIClient::Base::NotFoundError }
+      end
+    end
+
+    context 'using an id that exists' do
+      let(:id) { create_author.id }
+      let(:name) { 'Macadamia' }
+
+      it 'finds the model' do
+        expect(fetch_author(id).name).to eq(name)
       end
     end
   end
