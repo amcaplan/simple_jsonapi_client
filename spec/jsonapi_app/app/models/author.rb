9729e77fcc6814850de8682479d29d719812975e
Ariel Caplan <ariel.caplan@vitals.com>
2017-08-21 19:32:02 +0300

    Fix up has_manys to be dependent: :destroy

--- spec/jsonapi_app/app/models/author.rb
+++ spec/jsonapi_app/app/models/author.rb
@@ -1,6 +1,6 @@
 class Author < ApplicationRecord
-  has_many :posts
-  has_many :comments
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :name, length: { maximum: 50 }
 end
--- spec/jsonapi_app/app/models/post.rb
+++ spec/jsonapi_app/app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :author, optional: true
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
